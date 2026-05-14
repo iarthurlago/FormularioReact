4cd9b8e41b941844f610f46343ca765f8298d53d
iarthurlago <iarthur.almeida@gmail.com>
2026-05-14 10:32:30 -0300

    feat: commit de hoje

--- src/Componentes/FormularioCadastro.jsx
+++ src/Componentes/FormularioCadastro.jsx
@@ -4,70 +4,106 @@ import Contador from './Contador';
 import { useState } from "react";
 
 function FormularioCadastro(){
-    const [nome, setnome] = useState('')
-    const [email, setEmail] = useState('')
-    const [telefone, setTelefone] = useState('')
-    const [erro, setErro] =useState("");
-    const [sucesso, setSucesso] =useState(false);
+    // const [nome, setnome] = useState('')
+    // const [email, setEmail] = useState('')
+    // const [telefone, setTelefone] = useState('')
+    // const [erro, setErro] =useState("");
+    // const [sucesso, setSucesso] =useState(false);
+    const [user, setUser] = useState({nome:"", email:"", telefone:''})
+    const [verificacao, setVerificacao] = useState({erro:"", sucesso: false})
 
     const handleSubmit = (e) => {
-        e.preventdefault()
+        e.preventDefault()
         
-        if (nome.trim === "")
-          setErro("O campo desse nome está sem nada")
-        console.log({erro})  //function for dont recharge page.
+        if (user.nome.trim() === ""){
+        setVerificacao((dados) => ({
+          ...dados,
+          erro: "O campo desse nome está sem nada"
+        }))
+        console.log(verificacao.erro)  //function for dont recharge page.
         return
         }
 
-        setErro("")
-        setSucesso(true)
-        console.log({nome, email, telefone}) // Send for DB
-
-    const handleSubmit = (e) =>{
-      e.preventdefault()
-
-      if(nome.length != 11)
-        setErro("O campo necessita de 11 digitos")
-        console.log({erro})
+        if(user.telefone.trim().length != 11){
+        setVerificacao((dados) => ({
+            ...dados,
+            erro: "O campo necessita de 11 digitos"
+        }))
+        console.log(verificacao.erro)
         return
       }
 
-      setErro("")
-      setSucesso(true)
-      console.log({nome, email, telefone}) // Send for DB
+       if(!user.email){
+        setVerificacao((dados) => ({
+            ...dados,
+            erro: "email está incorreto"
+        }))
+        console.log(verificacao.erro)
+        return
+
+        } 
+        if(user.email.trim().length != 9){
+          setVerificacao((dados) => ({
+              ...dados,
+              erro: "email está incompleto"
+          }))
+          console.log(verificacao.erro)
+          return
+          }
+      
+
+        setVerificacao("")
+        setVerificacao((dados) => ({
+          ...dados,
+          sucesso: true
+        }))
+        console.log(user) // Send for DB
+    }
+
+
+
     return(
       
       <form onSubmit={handleSubmit}>
-        {erro && <p style={{color: "red"}}>{erro}</p>}
-        {sucesso && <p style={{color: "green"}}>Cadastrado !!</p>}
+        {verificacao.erro && <p style={{color: "red"}}>{verificacao.erro}</p>}
+        {verificacao.sucesso && <p style={{color: "green"}}>Cadastrado !!</p>}
           <InputField 
           Label={"Nome"} 
           type={"text"} 
           name={"nome"} 
           placeholder={"Digite Seu Nome Aqui"}  
-          value={nome} 
-          onChange={(e) => setnome(e.target.value)}/>
+          value={user.nome} 
+          onChange={(e) => setUser((dados) => ({
+            ...dados,
+            nome: e.target.value
+          }))}/>
 
           <InputField 
           Label={"Email"} 
           type={"email"} 
           name={"email"} 
           placeholder={"Digite Seu Email Aqui"}
-          value={email} 
-          onChange={(e) => setEmail(e.target.value)}/>
+          value={user.email} 
+          onChange={(e) => setUser((dados) => ({
+            ...dados,
+            email: e.target.value
+          }))}/>
 
           <InputField 
           Label={"Telefone"} 
           type={"number"} 
           name={"telefone"} 
           placeholder={"*****"}
-          value={telefone} 
-          onChange={(e) => setTelefone(e.target.value)}/>
+          value={user.telefone} 
+          onChange={(e) => setUser((dados) => ({
+            ...dados,
+            telefone: e.target.value
+          }))}/>
 
           <Button text="Clica"></Button>
 
         <div>
-            <p>Nome do usuario: {nome}</p>
+            <p>Nome do usuario: {user.nome}</p>
         </div>
           
       </form>
